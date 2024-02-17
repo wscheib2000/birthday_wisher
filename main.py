829fa14ed49b57952a96152caecbe6f66af86200
wscheib2000 <william.scheib@gatech.edu>
2024-02-17 18:02:44 -0500

    Actual starting files

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-import password as pwd
+##################### Extra Hard Starting Project ######################
 
-# smtplib.SMTP("smtp.gmail.com", port=587)
+# 1. Update the birthdays.csv
+
+# 2. Check if today matches a birthday in the birthdays.csv
+
+# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+
+# 4. Send the letter generated in step 3 to that person's email address.
+
+
+
+
